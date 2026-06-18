c7537e09c3d6964c184b13a8c51fc8e72ba1ac16
playrivorea <zakistki918@gmail.com>
2026-06-18 13:27:10 +0700

    Create page.tsx

---
+++ world/page.tsx
@@ -0,0 +1,11 @@
+import IslandView from "./IslandView";
+import HUD from "./HUD";
+
+export default function World() {
+  return (
+    <div>
+      <HUD />
+      <IslandView />
+    </div>
+  );
+}
