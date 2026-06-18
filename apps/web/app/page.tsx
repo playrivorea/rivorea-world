af98f2b08d9cff18c80e93b3e9b0f2ee01f92f5e
playrivorea <zakistki918@gmail.com>
2026-06-18 13:26:30 +0700

    Create page.tsx

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>🌊 Rivorea</h1>
+      <p>Start broke on a Mediterranean island. Rise to the top.</p>
+      <a href="/world">Enter World</a>
+    </main>
+  );
+}
